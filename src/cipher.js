ea412b2ca44526348cf2ee141ba490b9e54becdc
maryisabel <pacheco.maryisabel@gmail.com>
2018-05-31 08:32:46 -0500

    subiendo cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,45 @@
+/*El objeto windows cipher tendrá dos funciones (encode para cifrar y decode para descifrar), ambas
+tienen dos parámetros: offset (número de espacios) y string (el texto que se desea cifrar).*/
 window.cipher = {
-  // ... 
+  //Función encode para cifrar el texto.
+  encode : (offset,string) => {
+    string=string.toUpperCase();//Para que el texto ingresado se convierta siempre en mayúscula
+    let result = "";//El resultado.    
+    /*Usamos el bucle for, inicializamos el iterador(i) en 0; comprueba que i es menor que la longitud 
+    del texto ingresado; realiza la condicional if y else con éxito, e incrementa i en 1 después de
+    cada pase del bucle.*/
+    /*string.charCodeAt(i) para que devuelva un número indicando el valor unicode del carácter; así
+    obtenemos el número del código ASCII del texto ingresado.*/
+    for (i=0; i<string.length; i++){     
+        if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90) {//delimitamos en ASCII mayúsculas.
+            result += String.fromCharCode((string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65);
+            //result+= obtenemos la letra del código ASCII(Fórmula código ASCII que nos da un número)
+        } else {
+            result += string.charAt(i);//Si lo ingresado no es ninguna letra, que se muestre tal cual. 
+        }
+    }
+    return result;
+
+  },
+  //Función decode para descifrar el texto.
+  decode : (offset,string) => {
+    string=string.toUpperCase();
+    let result = "";
+    
+    for (i=0; i<string.length; i++){  
+        if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90) {
+            result += String.fromCharCode((string.charCodeAt(i) + 65- parseInt(offset)) % 26 + 65);
+        } else {
+        result += string.charAt(i); 
+        }
+    }
+    return result;
+  },
+   //Función cipherWithOffset recibe offset y devuelve objetos con métodos encode y decode
+   cipherWithOffset(offset) {
+    return window.ciphering = {
+      encode(string){return cipher.encode(offset, string);},//Método encode llamado en el resultado del index.js
+      decode(string){return cipher.decode(offset, string);}//Método decode llamado en el resultado del index.js
+    };
+  }
 };
---
+++ src/index.js
@@ -0,0 +1,22 @@
+//Declaramos las variables:
+let string = document.getElementById("texto");//string para el texto que se ingresará
+let offset = document.getElementById("spaces");//para el número de posiciones que el cifrado desplace cada caracter 
+
+//Evento del botón 1 (el que cifra), el resultado va a mostrar el texto cifrado.
+document.getElementById("boton1").addEventListener("click",()=>{
+    document.getElementById("resultado").value=cipher.encode(offset.value,string.value);
+});
+//Evento del botón 2 (el que descifra), el resultado va a mostrar el texto descifrado.
+document.getElementById("boton2").addEventListener("click",()=>{
+    document.getElementById("resultado").value=cipher.decode(offset.value,string.value);
+});
+//Llamando el botón del html a través de su ID "boton1" del botón de html 
+//y addEventListener tipo click para ejecutar función cifrar
+document.getElementById("boton1").addEventListener("click", ciframe);
+function ciframe() {//Function pulled from cipherWithOffset para el resultado
+  document.getElementById("resultado").innerHTML = (cipher.cipherWithOffset(offset)).encode(string);}
+//Llamando el botón del html a través de su ID "boton2" del botón de html 
+//y addEventListener tipo click para ejecutar función cifrar
+document.getElementById("bonton2").addEventListener("click", desciframe);
+function desciframe() {//Function pulled from cipherWithOffset para el resultado
+  document.getElementById("resultado").innerHTML = (cipher.cipherWithOffset(offset)).decode(string);}
